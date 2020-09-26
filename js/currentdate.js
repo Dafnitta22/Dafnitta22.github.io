4cd981de3ccbf08b46e7026104ab4de0bf9ddb8b
WDD 230 Test Student Account <maydafes@gmail.com>
2020-09-25 18:47:50 -0700

    Update

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,18 +1,9 @@
 
   //*************** */
   
-  // tolocaleDateString//
-  try {
-    let options = {
-      weekday: "short",
-      day: "numeric",
-      month: "numeric",
-      year: "numeric" 
-      };
-      document.getElementById("currentdate2").textContent= new Date().toLocaleDateString("en-US", options);
-      } catch (e) {
-      alert("Error with code or your browser does not support Locale");
-    
-  }
-  var x = document.lastModified;
-  document.getElementById("currentdate2").innerHTML = x;
+  
+let date = new Date();
+let modified = document.lastModified;
+
+document.getElementById("year").innerHTML = date.getFullYear();
+document.getElementById("date").innerHTML = modified;
