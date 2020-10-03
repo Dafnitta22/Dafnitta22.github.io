2d2983409c04eca68845c8df8889d85b0887d1a5
WDD 230 Test Student Account <maydafes@gmail.com>
2020-10-02 19:21:29 -0700

    update

---
+++ Lesson3/js/currentdate.js
@@ -0,0 +1,5 @@
+let date = new Date();
+let modified = document.lastModified;
+
+document.getElementById("year").innerHTML = date.getFullYear();
+document.getElementById("date").innerHTML = modified;
