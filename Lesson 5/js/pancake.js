42fd72a1a4ca13eef10f9c83c8e900a72ffbf945
WDD 230 Test Student Account <maydafes@gmail.com>
2020-10-17 19:57:16 -0700

    update

--- Lesson 5/js/pancake.js
+++ Lesson 5/js/pancake.js
@@ -1,6 +1,8 @@
 
-let d = d.getDay();
-if (d.getDay() == 4) {
-    alert('.message');
+let nd = new Date();
+let d = nd.getDay();
+
+if (d == 5) {
+    document.querySelector('.message').style.display = "block";
 }
 
