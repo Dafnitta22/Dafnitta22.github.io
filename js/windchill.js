26e26481b62351a34202b0b29c896c9ef8287165
WDD 230 Test Student Account <maydafes@gmail.com>
2020-10-24 14:42:44 -0700

    update

--- js/windchill.js
+++ js/windchill.js
@@ -1,10 +1,18 @@
 
 function windchill() {
-    let t= parseFloat(document.getElementById('curenttemp').textContent);
-    let s=13;
-    let output= "Wind Chill: " +(Math.round(f)) + "&#8457;"
-    if( t <= 50 && s >= 3) {
-    f=(35.74 + (0.6215*t) - (35.75*Math.pow(s,0.16)) + (0.4275*t*Math.pow(s,0.16)));
-    document. getElementById("output").innerHTML = output;  
+    let t = parseFloat(document.getElementById('currenttemp').innerHTML);
+    let s = parseFloat(document.getElementById('currentmph').innerHTML); 
+    let output =  totalwc(t,s);
+
+    if( t <= 50 && s > 3) {
+        document.getElementById("wc").innerHTML = output + (Math.round(f)) + "&#8457;";  
+    }
+    else {
+        document.getElementById("wc").innerHTML = N/A;  
+    }
 }
+function totalwc(t,s) {
+
+    let f=(35.74 + (0.6215*t)) - (35.75*(Math.pow(s, 0.16))) + (0.4275*(t*(Math.pow(s, 0.16))));
+    return f;
 }
