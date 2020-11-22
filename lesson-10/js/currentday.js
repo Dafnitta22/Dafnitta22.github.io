858f378d6d983b2ca937019c6d26514612cd8c9e
WDD 230 Test Student Account <maydafes@gmail.com>
2020-11-21 19:13:49 -0700

    update

--- lesson-10/js/currentday.js
+++ lesson-10/js/currentday.js
@@ -7,3 +7,5 @@ document.getElementById ('currentyear').innerHTML = year;
 
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 document.getElementById('currentdate').innerHTML = new Date().toLocaleDateString('en-US', options);
+
+
