0ea9e935fe1be5861fa57aaa22c59c506c193571
WDD 230 Test Student Account <maydafes@gmail.com>
2020-12-11 15:40:36 -0700

    update

--- scoots/js/weatherapi.js
+++ scoots/js/weatherapi.js
@@ -4,10 +4,10 @@ const apiURL ="https://api.openweathermap.org/data/2.5/weather?lat=20.501&lon=-8
 fetch(apiURL)
    .then((response) => response.json())
    .then((jsObject) => {
-       console.log(jsObject.cord[0]);
-document.getElementById('currenttemp').textContent = jsObject.cord[0].main.temp;
-document.getElementById('real').textContent= jsObject.cord[0].description;
-document.getElementById('humidity').textContent= jsObject.cord[0].main.humidity;
-document.getElementById('currents').textContent= jsObject.cord[0].wind.speed;
+       console.log(jsObject.coord[0]);
+document.getElementById('currenttemp').textContent = jsObject.coord[0].main.temp;
+document.getElementById('real').textContent= jsObject.coord[0].description;
+document.getElementById('humidity').textContent= jsObject.coord[0].main.humidity;
+document.getElementById('currents').textContent= jsObject.coord[0].wind.speed;
 
 });
